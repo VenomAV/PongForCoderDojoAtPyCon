d837c56bfad1429a352447445245280b6898968b
Andrea Vallotti <andrea.vallotti@gmail.com>
2018-04-07 15:44:00 +0200

    Hello, world!

---
+++ pong2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(args):
+    print("Hello, World!")
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

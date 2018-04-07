1ae6332565dd426c052aa2a0c34801851effcc80
Andrea Vallotti <andrea.vallotti@gmail.com>
2018-04-07 16:12:27 +0200

    Draw the first paddle

--- pong2.py
+++ pong2.py
@@ -5,6 +5,13 @@ import pygame
 
 SCREEN_SIZE = (640, 480)
 
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/paddle_vert.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
 class Game:
 
     RUNNING = 0
@@ -14,6 +21,7 @@ class Game:
         self.screen = self.init_game_window(SCREEN_SIZE, caption)
         self.backdrop = pygame.image.load('img/pong_a_2.bmp').convert()
         self.state = self.RUNNING
+        self.create_game_sprites()
 
     def is_game_over(self):
         return self.state == self.GAME_OVER
@@ -26,7 +34,9 @@ class Game:
 
     def render(self):
         self.screen.blit(self.backdrop,(0,0))
+        self.sprites.draw(self.screen)
         pygame.display.update()
+        pygame.time.delay(2)
         
     def terminate(self):
         pygame.quit()
@@ -36,12 +46,20 @@ class Game:
             if ourevent.type == pygame.QUIT:
                 self.state = self.GAME_OVER
 
+    def create_game_sprites(self):
+        self.sprites = pygame.sprite.Group()
+        self.sprites.add( Paddle( (100,100)))
+
+    def update(self):
+        self.sprites.update()
+
 def main(args):
     game = Game(SCREEN_SIZE, "python pong")
     
     while not game.is_game_over():
         game.render()
         game.handle_events()
+        game.update()
     
     game.terminate()
 

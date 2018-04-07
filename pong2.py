aa016c5b658b75e349b923763c0b8227d0912fad
Andrea Vallotti <andrea.vallotti@gmail.com>
2018-04-07 16:06:57 +0200

    Manage quit event, the right way

--- pong2.py
+++ pong2.py
@@ -6,9 +6,17 @@ import pygame
 SCREEN_SIZE = (640, 480)
 
 class Game:
+
+    RUNNING = 0
+    GAME_OVER = 1
+
     def __init__(self, SCREEN_SIZE, caption):
         self.screen = self.init_game_window(SCREEN_SIZE, caption)
         self.backdrop = pygame.image.load('img/pong_a_2.bmp').convert()
+        self.state = self.RUNNING
+
+    def is_game_over(self):
+        return self.state == self.GAME_OVER
         
     def init_game_window(self, screen_size, caption):
         pygame.init()
@@ -23,15 +31,17 @@ class Game:
     def terminate(self):
         pygame.quit()
 
+    def handle_events(self):
+        for ourevent in pygame.event.get():
+            if ourevent.type == pygame.QUIT:
+                self.state = self.GAME_OVER
+
 def main(args):
     game = Game(SCREEN_SIZE, "python pong")
     
-    terminate = False
-    while not terminate:
-        for ourevent in pygame.event.get():
-            if ourevent.type == pygame.QUIT:
-                terminate = True
+    while not game.is_game_over():
         game.render()
+        game.handle_events()
     
     game.terminate()
 
